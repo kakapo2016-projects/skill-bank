19d510e9f35ab8c38ab426c95e06a3c34c243e34
Nicholas r Maskell <nick@Nicholass-MacBook-Pro.local>
2016-02-26 12:03:49 +1300

    Basic appending is working

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,22 @@
 'use strict'
 var exphbs  = require('express-handlebars')
+var fs = require('fs')
+var path = require('path')
+
+var dataPath = path.join(__dirname, '../data/db.json')
+
 
 exports = module.exports = function (app) {
-  app.get('/', function (req, res) {
-    if (req.query) 
-    res.render('home')
+  app.get('/people', function (req, res) {
+    fs.readFile(dataPath, 'utf8', function (err, data) {
+      if (err) {
+        reject (err)
+      } else {
+        var people = JSON.parse(data).people
+
+        res.render('home', {people: people})
+      }
+    })
   })
 
   app.get('/create', function (req, res) {
@@ -12,3 +24,20 @@ exports = module.exports = function (app) {
   })
 
 }
+
+
+//GET / 
+  //blank homepage
+  // -> home -> with people
+
+  // GET people/
+    //  -> home -> with peoplw
+
+// GET people/:id
+ // -> profile.hNDLEBRS -> with a person 
+
+// DELETE people/:id
+
+// POST people/:id
+
+// PUT people/:id
