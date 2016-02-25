5c441f041d4d1415ea370d519845e918c40d2fe2
Jason Veng <email": null>
2016-02-26 12:11:02 +1300

    merge master

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var routes = require('./routes')
 var path = require('path')
 
 var app = express()
+var path = require('path')
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
@@ -11,6 +12,6 @@ app.set('view engine', 'handlebars')
 //Routes being called from my routes.js
 routes(app)
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.listen(3000);
